9a6dad3694bebb75990ee84e4cb2a76603d007da
Harry Yuhao Han <s2162783@ed.ac.uk>
2024-02-04 15:52:30 +0000

    log improve

--- main.go
+++ main.go
@@ -17,36 +17,68 @@ import (
 
 const fileDir = "/tmp/blogDownloadServer/"
 const logFileDir = "/tmp/blogDownloadServer_log/"
-const logFilePath = "/tmp/blogDownloadServer_log/log"
 const gitURL = "https://github.com/harryhanYuhao/blogPosts.git"
+
+func ReadUserIP(r *http.Request) string {
+	IPAddress := r.Header.Get("X-Real-Ip")
+	if IPAddress == "" {
+		IPAddress = r.Header.Get("X-Forwarded-For")
+	}
+	if IPAddress == "" {
+		IPAddress = r.RemoteAddr
+	}
+	return IPAddress
+}
 
 func formatedCurTime() string {
 	return time.Now().Format("2006-01-02 15:04:05")
 }
 
-func log_writeLog(input string) {
-	execBash.Execute("mkdir -p " + logFileDir)
-	f, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+func writeLog(path string, filename string, input string) {
+
+	filepath := path + filename
+	// check if file exist
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		execBash.Execute("mkdir -p " + path)
+	}
+
+	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Fail to Open " + filepath)
 	}
 	defer f.Close()
 
 	if _, err := f.WriteString(input + "\n"); err != nil {
-		log.Fatal(err)
+		fmt.Println("Fail to Write to " + filepath)
 	}
 }
 
-func syncBlog() {
+func logSyncBlog(input string, ip string) {
+	message := formatedCurTime() + " " + ip + " " + input
+	writeLog(logFileDir, "sync_log", message)
+}
+
+func logDownload(input string, ip string) {
+	message := formatedCurTime() + " " + ip + " " + input
+	writeLog(logFileDir, "download_log", message)
+}
+
+func logList(input string, ip string) {
+	message := formatedCurTime() + " " + ip + " " + input
+	writeLog(logFileDir, "list_log", message)
+}
+
+func syncBlog(ip string) {
 	_, err := os.Stat(fileDir)
 	if os.IsNotExist(err) {
 		// TODO: add err check
 		execBash.Execute("mkdir -p " + fileDir)
 		execBash.Execute("git -C " + fileDir + " clone " + gitURL + " " + fileDir)
-		log_writeLog("Repo" + fileDir + " Created: " + formatedCurTime())
+		logSyncBlog("Repo "+fileDir+" Created ", ip)
 	} else {
 		execBash.Execute("git -C " + fileDir + " pull")
-		log_writeLog("Repo " + fileDir + " Updated: " + formatedCurTime())
+		logSyncBlog("Repo "+fileDir+" Updated ", ip)
 	}
 }
 
@@ -56,8 +88,9 @@ func handleSync(w http.ResponseWriter, r *http.Request) {
 	// check key
 	if crypto.Key == fmt.Sprintf("%x", idsum) {
 		w.Write([]byte("Authorized"))
-		syncBlog()
+		syncBlog(ReadUserIP(r))
 	} else {
+		logSyncBlog("Unauthorized sync request", ReadUserIP(r))
 		w.Write([]byte("Unauthorized"))
 	}
 }
@@ -73,14 +106,20 @@ func handleList(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(list)
+	logList("List Request", ReadUserIP(r))
+}
 
+func handleDownload(w http.ResponseWriter, r *http.Request) {
+	filename := r.URL.Path[len("/download/"):]
+	http.ServeFile(w, r, fileDir+filename)
+	logDownload("Request for "+filename, ReadUserIP(r))
 }
 
 func main() {
-	syncBlog()
+	syncBlog("localhost")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/sync/", handleSync)
 	mux.HandleFunc("/list/", handleList)
-	mux.Handle("/download/", http.StripPrefix("/download/", http.FileServer(http.Dir(fileDir))))
+	mux.HandleFunc("/download/", handleDownload)
 	log.Fatal(http.ListenAndServe(":10001", mux))
 }
